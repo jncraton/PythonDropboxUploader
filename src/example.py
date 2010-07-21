42c6e404c8cca5781234b7ea26613f856df5ef8b
Jon Craton <jncraton@gmail.com>
2010-07-21 18:32:47 +0000

    added message to end of example code

--- src/example.py
+++ src/example.py
@@ -5,3 +5,5 @@ email = raw_input("Enter Dropbox email address:")
 password = getpass("Enter Dropbox password:")
 
 upload_file("example.py","/","dbupload_test.txt",email,password)
+
+print("Uploaded dbupload_test.txt to the root of your Dropbox")
