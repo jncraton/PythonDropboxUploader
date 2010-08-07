345b12cab218cd12dde478535f50e9776d016cb4
Jon Craton <jncraton@gmail.com>
2010-08-07 12:16:10 +0000

    minor changes to attempt to fix a bug

--- src/dbupload/upload.py
+++ src/dbupload/upload.py
@@ -10,17 +10,17 @@ def upload_file(local_file,remote_dir,remote_file,email,password):
     br.open('https://www.dropbox.com/login')
     
     # Enter the username and password into the login form
-    isLoginForm = lambda f: f.action == "https://www.dropbox.com/login"
+    isLoginForm = lambda l: l.action == "https://www.dropbox.com/login"
     br.select_form(predicate=isLoginForm)
     
-    br["login_email"] = email
-    br["login_password"] = password
+    br.form['login_email'] = email
+    br.form['login_password'] = password
     
     # Send the form
     response = br.submit()
     
     # Add our file upload to the upload form once logged in
-    isUploadForm = lambda f: f.action == "https://dl-web.dropbox.com/upload"
+    isUploadForm = lambda u: u.action == "https://dl-web.dropbox.com/upload"
     br.select_form(predicate=isUploadForm)
     br.form.find_control("dest").readonly = False
     br.form.set_value(remote_dir,"dest")
