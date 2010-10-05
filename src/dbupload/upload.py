49e5214b174dba3405f25a0617f2fdf0ad06fea1
Jon Craton <jncraton@gmail.com>
2010-10-04 20:11:40 -0400

    - added a little better error checking

--- src/dbupload/upload.py
+++ src/dbupload/upload.py
@@ -11,7 +11,12 @@ def upload_file(local_file,remote_dir,remote_file,email,password):
     
     # Enter the username and password into the login form
     isLoginForm = lambda l: l.action == "https://www.dropbox.com/login" and l.method == "POST"
-    br.select_form(predicate=isLoginForm)
+    
+    try:
+        br.select_form(predicate=isLoginForm)
+    except:
+        print("Unable to find login form.");
+        exit(1);
     
     br['login_email'] = email
     br['login_password'] = password
@@ -21,7 +26,14 @@ def upload_file(local_file,remote_dir,remote_file,email,password):
     
     # Add our file upload to the upload form once logged in
     isUploadForm = lambda u: u.action == "https://dl-web.dropbox.com/upload" and u.method == "POST"
-    br.select_form(predicate=isUploadForm)
+
+    try:
+        br.select_form(predicate=isUploadForm)
+    except:
+        print("Unable to find upload form.");
+        print("Make sure that your login information is correct.");
+        exit(1);
+        
     br.form.find_control("dest").readonly = False
     br.form.set_value(remote_dir,"dest")
     br.form.add_file(open(local_file),"",remote_file)
