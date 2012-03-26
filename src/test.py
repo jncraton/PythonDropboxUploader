b2dbc4e6ccec7420b534218d11f5179bf9a56877
jncraton <jncraton@gmail.com>
2012-03-26 16:58:06 -0400

    added simple test suite

---
+++ src/test.py
@@ -0,0 +1,41 @@
+from dbupload import upload_file, DropboxConnection
+from getpass import getpass
+import unittest
+import mechanize
+
+email = ""
+password = ""
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        fh = open("small_test_file.txt","w")
+        fh.write("Small test file")
+        fh.close()
+        
+    def test_login(self):
+        conn = DropboxConnection(email, password)
+        
+        self.assertTrue(conn.is_logged_in())
+    
+    def test_bad_login(self):
+        try:
+            conn = DropboxConnection("bad email", "bad password")
+        except:
+            self.assertRaises(Exception)
+    
+    def test_upload_small(self):
+        conn = DropboxConnection(email, password)
+        conn.upload_file("small_test_file.txt","/","small_test_file.txt")
+        
+    def test_dir_list(self):
+        conn = DropboxConnection(email, password)
+        conn.get_dir_list("/Archives")
+    
+    def test_download(self):
+        conn = DropboxConnection(email, password)
+        conn.download_file("/","small_test_file.txt","small_test_file.txt")
+        
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSequenceFunctions)
+    unittest.TextTestRunner(verbosity=2).run(suite)
