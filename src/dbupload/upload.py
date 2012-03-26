b67c970ea093443a6867bfec9edb4353264d6716
jncraton <jncraton@gmail.com>
2012-03-26 16:57:00 -0400

    updated old interface to use new class

--- src/dbupload/upload.py
+++ src/dbupload/upload.py
@@ -1,46 +1,8 @@
 import mechanize
-
-def login(email,password):
-    """ Login to Dropbox and return mechanize browser instance """
-    
-    # Fire up a browser using mechanize
-    br = mechanize.Browser()
-    
-    # Browse to the login page
-    br.open('https://www.dropbox.com/login')
-    
-    # Enter the username and password into the login form
-    isLoginForm = lambda l: l.action == "https://www.dropbox.com/login" and l.method == "POST"
-    
-    try:
-        br.select_form(predicate=isLoginForm)
-    except:
-        raise(Exception('Unable to find login form'))
-    
-    br['login_email'] = email
-    br['login_password'] = password
-    
-    # Send the form
-    response = br.submit()
-    
-    return br
+from dbconn import DropboxConnection
 
 def upload_file(local_file,remote_dir,remote_file,email,password):
     """ Upload a local file to Dropbox """
     
-    br = login(email, password)
-
-    # Add our file upload to the upload form once logged in
-    isUploadForm = lambda u: u.action == "https://dl-web.dropbox.com/upload" and u.method == "POST"
-
-    try:
-        br.select_form(predicate=isUploadForm)
-    except:
-        raise(Exception('Unable to find upload form'))
-        
-    br.form.find_control("dest").readonly = False
-    br.form.set_value(remote_dir,"dest")
-    br.form.add_file(open(local_file,"rb"),"",remote_file)
-    
-    # Submit the form with the file
-    br.submit()
+    conn = DropboxConnection(email, password)
+    conn.upload_file(local_file,remote_dir,remote_file)
