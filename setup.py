095b959edda119cf052977e43bb42fa7b9cf83b5
Pete Bachant <petebachant@gmail.com>
2014-08-26 15:04:18 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+    name='dbupload',
+    author='Jon Craton',
+    packages=['dbupload'],
+    scripts=[],
+    license='license.txt',
+    long_description=open('readme.md').read()
+)
